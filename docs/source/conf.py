9c063f529d20386f4fdbe681de233c90346bde5d
Arkadiusz Wtykło <arek.wtyklo@arteneoit.pl>
2021-10-14 13:43:16 +0200

    WIP: Add make files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,4 +7,13 @@ html_theme_options = {
     'color_primary': 'blue',
     'globaltoc_depth': 1,
     'globaltoc_collapse': False,
+    'version_dropdown': True,
+    'version_info': {
+        'development': 'development',
+        'stable': '',
+        'v2.0.0': 'v2.0.0',
+        'v2.1.0': 'v2.1.0',
+        'v2.1.5': 'v2.1.5',
+        'v2.4.0': 'v2.4.0',
+    },
 }
